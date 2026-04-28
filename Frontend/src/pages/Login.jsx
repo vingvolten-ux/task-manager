cce500461eca29edb602af08657f11bd10404203
Reph_193 <vingvolten@gmail.com>
2026-04-28 03:03:55 +0200

    commit 3AM

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 const login = async () => {
-  const res = await fetch("https://your-backend.onrender.com/login", {
+  const res = await fetch("postgresql://task_manager_12h6_user:zHJF7YFIjSho7XUR0uqRXpll7fHFcHj3@dpg-d7ntcku8bjmc7392vg90-a/task_manager_12h6", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: {
     rejectUnauthorized: false,
   },
